588fe5ca0f3d4285a0b0ed5c4d09309fe7ee1383
olliebowe <93463703+olliebowe@users.noreply.github.com>
2022-02-21 20:17:31 +1100

    constructor added

--- photowall/src/Photo.js
+++ photowall/src/Photo.js
@@ -1,20 +1,23 @@
 import React from "react";
-import { Component } from "react";
 
-class Photo extends Component{
 
-    render(){
-        
-        const post= this.props.post
+
+function Photo(props){
+    const post= props.post
         return (
             <figure className='figure'>
                 <img className='photo' src={post.imageLink} alt={post.description}/>
                 <figcaption className='caption'> <p>{post.description}</p></figcaption>
+                <div className='button-container'>
+                    <button className='remove-button'>Remove</button>
+
+                </div>
             </figure>
         )
-
+  
         
-    }
+        
+    
 }
 
 export default Photo
